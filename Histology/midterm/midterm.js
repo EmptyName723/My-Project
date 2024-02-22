4baeb16af498dac803819727023bbf878a8c8fd0
EmptyName723 <154806953+EmptyName723@users.noreply.github.com>
2024-02-22 21:34:38 +0800

    Update midterm.js

--- Histology/midterm/midterm.js
+++ Histology/midterm/midterm.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const IDList = ["0","1127021375"];
+    const IDList = ["0","1663373183","973606625","1127021375","862779847"];
     const nameData = [];
 
     function fetchData(id) {
@@ -48,7 +48,10 @@ document.addEventListener("DOMContentLoaded", function () {
       });
 
     let epitheliumData = [];
+    let connectiveData = [];
+    let boneData = [];
     let muscleData = [];
+    let nerveData = [];
 
     let quiz = [];
     let selectedQuestion;
@@ -66,14 +69,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function updateQuiz() {
       epitheliumData = nameData[0];
-      muscleData = nameData[1];
+      connectiveData = nameData[1];
+      boneData = nameData[2];
+      muscleData = nameData[3];
+      nerveData = nameData[4];
 
       quiz = Array.from(checkBoxes)
         .filter(elm => elm.checked)
         .flatMap(elm => {
           const dataId = elm.getAttribute('data-id');
           if (dataId === 'epithelium_') return epitheliumData;
+          if (dataId === 'connective_') return connectiveData;
+          if (dataId === 'bone_') return boneData;
           if (dataId === 'muscle_') return muscleData;
+          if (dataId === 'nerve_') return nerveData;
           return [];
         });
 
